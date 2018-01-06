cd85b5691c52403a62c053b3a37d2587580384d1
cornwarecjp <cornwarecjp@ultimatestunts.nl>
2018-01-06 14:56:49 +0100

    Add copyright header, and make file executable

--- bf.py
+++ bf.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+#    bf.py
+#    Copyright (C) 2017-2018 by CJP
+#
+#    This file is part of Brainfuck-compiler-interpreter.
+#
+#    Brainfuck-compiler-interpreter is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Brainfuck-compiler-interpreter is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Brainfuck-compiler-interpreter. If not, see <http://www.gnu.org/licenses/>.
+
 from collections import defaultdict
 import sys
 from collections import deque
